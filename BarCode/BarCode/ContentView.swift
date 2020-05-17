8aaf7d278fbb65e27c3c34b3693656daf57a622f
Dempsey Salazar <>
2020-05-17 11:29:03 -0400

    First build

---
+++ BarCode/BarCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarCode
+//
+//  Created by Dempsey Salazar on 5/17/20.
+//  Copyright © 2020 Dempsey Salazar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ BarCode/BarCode/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  BarCode
+//
+//  Created by Dempsey Salazar on 5/17/20.
+//  Copyright © 2020 Dempsey Salazar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
